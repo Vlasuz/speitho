5c0e182fdb127b12ba2cf1515b2a1a100f47d469
Vlas Zubenko <vlaszubenko@MacBook-Air-Vlas.local>
2022-05-08 10:18:57 +0300

    v1.0

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Wow JS
+
+if(document.querySelector('.wow'))
+	new WOW().init();
+
+// Wow JS
+
+
+
+$(document).ready(function () {
+
+	
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
