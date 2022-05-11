4930f3d5e4a909bfbaa8993ba43b7b5023e45157
Vlas Zubenko <vlaszubenko@MacBook-Air-Vlas.local>
2022-05-11 13:43:09 +0300

    v1.1

--- js/main.js
+++ js/main.js
@@ -6,11 +6,24 @@ if(document.querySelector('.wow'))
 // Wow JS
 
 
+let akkordion = document.querySelectorAll('.akkordion')
+let cc = 0
 
-$(document).ready(function () {
+akkordion.forEach((item) => {
 
-	
+	let itemHeight = item.querySelector('.akkordion__body').offsetHeight
+	item.querySelector('.akkordion__body').style.height = '0px'
 
+
+	item.querySelector('.akkordion__head').onclick = function () {
+		this.closest('.akkordion').classList.toggle('akkordion_active')
+		if( this.closest('.akkordion').classList.contains('akkordion_active') ){
+			this.closest('.akkordion').querySelector('.akkordion__body').style.height = itemHeight+'px';
+		}
+		else{
+			this.closest('.akkordion').querySelector('.akkordion__body').style.height = '0px';
+		}
+	}
 })
 
 
